1409d7007d4e9645209a0d0995a9c88480da1834
Carl Lei <xecycle@gmail.com>
2016-07-14 15:15:59 +0800

    Binary string literal by operator""

--- bin-str-literal.cc
+++ bin-str-literal.cc
@@ -8,11 +8,19 @@ std::string bin_string(const char (&str)[N])
   return { str, str+N-1 };
 }
 
+std::string operator"" _bin(const char* str, size_t size)
+{
+  return { str, str+size };
+}
+
 int main()
 {
 
   auto s = bin_string("foo\0bar");
   assert(s.size() == 7);
+
+  auto s2 = "foo\0bar"_bin;
+  assert(s2.size() == 7);
 
   return 0;
 }
