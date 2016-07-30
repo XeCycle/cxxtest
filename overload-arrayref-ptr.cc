5f20cd320e6f83bfcda9c1a29c542437d0b08dc6
Carl Lei <xecycle@gmail.com>
2016-07-30 08:43:57 +0800

    hack a preference into arrayref/ptr selection

--- overload-arrayref-ptr.cc
+++ overload-arrayref-ptr.cc
@@ -12,10 +12,34 @@ int f(const char*)
   return -1;
 }
 
+struct fallback {};
+struct preferred : fallback {};
+
+int g_impl(fallback, const char*)
+{
+  return -1;
+}
+
+template <size_t N>
+int g_impl(preferred, const char(&)[N])
+{
+  return N;
+}
+
+template <class T>
+int g(T&& x)
+{
+  return g_impl(preferred(), (T&&)x);
+}
+
 int main()
 {
 
   assert(f("abc") == -1);
+
+  assert(g("abc") == 4);
+  const char* str = "abc";
+  assert(g(str) == -1);
 
   return 0;
 }
