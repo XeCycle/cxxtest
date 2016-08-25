c93afa02b37a1d5d1e025c56189ef667393c35ff
Carl Lei <xecycle@gmail.com>
2016-08-25 18:59:51 +0800

    typo

--- context.cc
+++ context.cc
@@ -4,7 +4,7 @@ auto f(T) -> decltype(g(T()));
 int g(int);
 
 #ifdef HAS_F_DEFINITION_IN_THIS_TU
-// clang accepts this file this is defined, rejects otherwise;
+// clang accepts this file if this is defined, rejects otherwise;
 // gcc rejects in both cases
 template <class T>
 auto f(T) -> decltype(g(T())) { return g(T()); }
