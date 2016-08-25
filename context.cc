6a1d3badbcbdb4da5e1059b9f5acfb5d8ef9aeda
Carl Lei <xecycle@gmail.com>
2016-08-25 18:58:09 +0800

    Lookup context of trailing return type expression?

---
+++ context.cc
@@ -0,0 +1,16 @@
+template <class T>
+auto f(T) -> decltype(g(T()));
+
+int g(int);
+
+#ifdef HAS_F_DEFINITION_IN_THIS_TU
+// clang accepts this file this is defined, rejects otherwise;
+// gcc rejects in both cases
+template <class T>
+auto f(T) -> decltype(g(T())) { return g(T()); }
+#endif
+
+int main()
+{
+  return f(0);
+}
